25306fd820b6795ff0d5c6522bc890c1db9615fc
angiecortez <angiecorteztay1@hotmail.com>
2018-05-31 12:21:37 -0500

    agregando encode y decode

--- src/cipher.js
+++ src/cipher.js
@@ -1,7 +1,43 @@
 window.cipher = {
-  const encrypt = function encrypt (){
-    document.addEventListener("onclick")
-  }
+  encode : (string, offset) => {
+    let wordCif = "";
+    string = string.toUpperCase();
+    for(let i = 0; i < string.length; i++)// for(let blabla of string)
+    {
+      let textChar = string.charCodeAt(i);
+      if(textChar >= 65 && textChar <= 90){
+        wordCif += String.fromCharCode((textChar - 65 + parseInt(offset))%26+65);
+       }
+      // else if (textChar >= 97 && textChar <= 122) {
+      //   wordCif  += String.fromCharCode((textChar - 97 + parseInt(offset))%26+97);
+      //   }
+      // else if (textChar == 32){
+      //   wordCif += " ";
+      // }
+    }
+   //retornando el valor
+   return wordCif;
+ },
 
-  // ...
-};
+//funcion Decipher
+  decode : (string, offset) => {
+    let wordNoCif = "";
+    string = string.toUpperCase();
+    for(let i = 0; i < string.length; i++){
+      let textChar = string.charCodeAt(i);
+      if(textChar >= 65 && textChar <= 90) {
+      let upper = String.fromCharCode((textChar + 65 - parseInt(offset))%26+65);
+      wordNoCif += upper;
+      }
+      // else if (textChar >= 97 && textChar <= 122) {
+      // let lower = String.fromCharCode((textChar + 97 - parseInt(offset)-12)%26+97);
+      //   wordNoCif += lower;
+      //   }
+      // else if (textChar == 32) {
+      //   wordNoCif += " ";
+      // }
+    }
+  return wordNoCif;
+}
+
+}
--- src/index.js
+++ src/index.js
@@ -1,4 +1,18 @@
-const cifrado = function(cifrado, numero){
-  let salida = ""
-  for(let i = 0; i < cifrado.length; i++)
-}
+const btn1 = document.getElementById('cifr');
+const btn2 = document.getElementById('decifr');
+const key = document.getElementById('offset');
+const answer = document.getElementById('answer');
+const message = document.getElementById('message');
+
+
+
+ btn1.addEventListener('click', () => {
+  let xxx = message.value;
+  let yyy = key.value;
+  answer.textContent = window.cipher.encode(xxx,yyy);
+});
+btn2.addEventListener('click', () => {
+ let xxx = message.value;
+ let yyy = key.value;
+ answer.textContent = window.cipher.decode(xxx,yyy);
+});
